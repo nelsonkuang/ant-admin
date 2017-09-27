9f3d0e8a7970dc798f929bdb48bf1c4182ff9f13
NelsonKuang <410374308@qq.com>
2017-09-27 12:31:40 +0800

    优化sider-menu抽象为无状态组件

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Route, Link, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { Layout, Menu, Breadcrumb, Badge, Icon, Avatar } from 'antd';
 import Home from './containers/Home';
 import Tables from './containers/Tables';
+import SiderMenu from './components/layout/SiderMenu';
 import './App.css';
 
 const { SubMenu } = Menu;
@@ -65,43 +66,7 @@ class App extends Component {
             collapsed={this.state.collapsed}
             onCollapse={this.onCollapse}
           >
-            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-              <Menu.Item key="1">
-                <Link to="/">
-                  <Icon type="pie-chart" />
-                  <span>面板</span>
-                </Link>
-              </Menu.Item>
-              <Menu.Item key="2">
-                <Link to="/tables">
-                  <Icon type="switcher" />
-                  <span>表格</span>
-                </Link>
-              </Menu.Item>
-              <Menu.Item key="3">
-                <Icon type="desktop" />
-                <span>设备</span>
-              </Menu.Item>
-              <SubMenu
-                key="sub1"
-                title={<span><Icon type="user" /><span>个人中心</span></span>}
-              >
-                <Menu.Item key="4">Tom</Menu.Item>
-                <Menu.Item key="5">Bill</Menu.Item>
-                <Menu.Item key="6">Alex</Menu.Item>
-              </SubMenu>
-              <SubMenu
-                key="sub2"
-                title={<span><Icon type="team" /><span>组织关系</span></span>}
-              >
-                <Menu.Item key="7">Team 1</Menu.Item>
-                <Menu.Item key="8">Team 2</Menu.Item>
-              </SubMenu>
-              <Menu.Item key="9">
-                <Icon type="file" />
-                <span>文件</span>
-              </Menu.Item>
-            </Menu>
+            <Route path="*" component={SiderMenu}/>
           </Sider>
           <Layout style={{ padding: '0 10px 10px' }}>
             <Breadcrumb style={{ margin: '12px 0' }}>
