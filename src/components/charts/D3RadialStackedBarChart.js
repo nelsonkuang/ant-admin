293c4d7543154189d505b3ad20fd262cdffea5a1
NelsonKuang <410374308@qq.com>
2017-10-23 09:57:02 +0800

    添加径向堆栈柱状图动效及tips提示

--- src/components/charts/D3RadialStackedBarChart.js
+++ src/components/charts/D3RadialStackedBarChart.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 import * as d3 from 'd3';
-import * as scaleRadial from '../../utils/d3-scale-radial';
 import * as d3Tip from 'd3-tip';
+import * as scaleRadial from '../../utils/d3-scale-radial';
 
 class D3RadialStackedBarChart extends React.Component {
     componentDidMount() {
@@ -17,13 +17,13 @@ class D3RadialStackedBarChart extends React.Component {
 
         let chart = d3.select(this.chartRef).attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom);  
 
-        let x = d3.scaleBand()
+        let x = d3.scaleBand() // 定义x轴
             .range([0, 2 * Math.PI])
             .align(0);
 
-        let y = scaleRadial().range([innerRadius, outerRadius]);
+        let y = scaleRadial().range([innerRadius, outerRadius]); // 定义y轴
 
-        let z = d3.scaleOrdinal()
+        let z = d3.scaleOrdinal() //通用颜色
             .range(d3.schemeCategory10);      
 
         const data = this.props.data;
@@ -35,19 +35,42 @@ class D3RadialStackedBarChart extends React.Component {
             q4:'第四季度'
         };
 
-        let series = d3.stack()
+        let series = d3.stack() // 定义堆栈图
             .keys(keys)
             .offset(d3.stackOffsetDiverging)(data);
 
-        x.domain(data.map(function(d) { return d.city; }));
+        x.domain(data.map(function(d) { return d.city; })); // x与y轴的值域
 
         y.domain([0, d3.max(series, function(serie){
             return d3.max(serie, function(d) { return d[1]; });        
         })]);
 
+        let tip = d3Tip() // 设置tip
+            .attr('class', 'd3-tip stacked-demo')
+            .offset([-10, 0])
+            .html(function (d) {
+              　let total = d.data.q1 + d.data.q2 + d.data.q3 + d.data.q4;
+                return '<strong>' + d.data.city + '</strong><br>'
+                + '<span style="color:'+ z(keys[0]) +'">' + names.q1 + ': ' + d.data.q1 + ' 亿</span><br>'
+                + '<span style="color:'+ z(keys[1]) +'">' + names.q2 + ': ' + d.data.q2 + ' 亿</span><br>'
+                + '<span style="color:'+ z(keys[2]) +'">' + names.q3 + ': ' + d.data.q3 + ' 亿</span><br>'
+                + '<span style="color:'+ z(keys[3]) +'">' + names.q4 + ': ' + d.data.q4 + ' 亿</span><br>'
+                + '<span style="color:#fff">年总: ' + total + ' 亿</span>';
+            });
+
+        chart.call(tip);
+
         let g = chart.append("g").attr("transform", "translate(" + containerWidth / 2 + "," + containerHeight * 0.5 + ")"); // 设最外包层在总图上的相对位置
 
-        let label = g.append("g")
+        chart.append("defs").append("clipPath") // 添加圆形遮罩
+            .attr("id", "clip")
+          　.append("circle")
+            .attr("r", 0)            
+            .transition()
+            .duration(800)
+            .attr("r", outerRadius);
+
+        let label = g.append("g") // 画x轴
           .selectAll("g")
           .data(data)
           .enter().append("g")
@@ -62,7 +85,7 @@ class D3RadialStackedBarChart extends React.Component {
             .attr("transform", function(d) { return (x(d.city) + x.bandwidth() / 2 + Math.PI / 2) % (2 * Math.PI) < Math.PI ? "rotate(90)translate(0,16)" : "rotate(-90)translate(0,-9)"; })
             .text(function(d) { return d.city; });
 
-        let yAxis = g.append("g")
+        let yAxis = g.append("g") // 画y轴圈圈及文字
             .attr("text-anchor", "end");
 
         let yTick = yAxis
@@ -101,10 +124,14 @@ class D3RadialStackedBarChart extends React.Component {
           .selectAll("g")
           .data(series)
           .enter().append("g")
+            .attr('clip-path', 'url(#clip)')
             .attr("fill", function(d) { return z(d.key); })
             .selectAll("path")
             .data(function(d) { return d; })
             .enter().append("path")
+              .on('mouseover', tip.show)
+            　.on('mouseout', tip.hide)
+              .attr("cursor", "pointer")
               .attr("d", d3.arc()
                   .innerRadius(function(d) { return y(d[0]); })
                   .outerRadius(function(d) { return y(d[1]); })
@@ -113,7 +140,7 @@ class D3RadialStackedBarChart extends React.Component {
                   .padAngle(0.01)
                   .padRadius(innerRadius)); 
 
-        let legend = g.append("g")
+        let legend = g.append("g") // 画legend
           .selectAll("g")
           .data(keys.slice())
           .enter().append("g")
